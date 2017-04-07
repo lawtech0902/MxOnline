b604422a333eed6506ea46fb2ae558d9988858a8
Lawtech <584563542@qq.com>
2017-04-07 16:14:38 +0800

    xadmin集成富文本ueditor

--- apps/courses/adminx.py
+++ apps/courses/adminx.py
@@ -5,7 +5,7 @@ __date__ = '2017/3/1 下午8:19'
 """
 
 from .models import Course, CourseResource, Lesson, Video, BannerCourse
-
+from organization.models import CourseOrg
 import xadmin
 
 
@@ -20,20 +20,33 @@ class CourseResourceInline(object):
 
 
 class CourseAdmin(object):
-    list_display = ['name', 'desc', 'detail', 'degree', 'learned_times', 'students', 'fav_nums', 'image', 'click_nums',
-                    'add_time']
+    list_display = ['name', 'desc', 'detail', 'degree', 'learned_times', 'students', 'fav_nums', 'click_nums',
+                    'add_time', 'get_zj_nums', 'go_to']
     search_fields = ['name', 'desc', 'detail', 'degree', 'learned_times', 'students', 'fav_nums', 'image', 'click_nums']
     list_filter = ['name', 'desc', 'detail', 'degree', 'learned_times', 'students', 'fav_nums', 'image', 'click_nums',
                    'add_time']
     ordering = ['-click_nums']
     readonly_fields = ['click_nums']
+    list_editable = ['desc', 'degree']
     exclude = ['fav_nums']
     inlines = [LessonInline, CourseResourceInline]
+    style_fields = {'detail': 'ueditor'}
+
+    # refresh_times = [3, 5]  # 每3秒或5秒自动刷新一次页面
 
     def queryset(self):
         qs = super(CourseAdmin, self).queryset()
         qs = qs.filter(is_banner=False)
         return qs
+
+    def save_models(self):
+        # 在保存课程的时候统计课程机构的课程数
+        obj = self.new_obj
+        obj.save()
+        if obj.course_org is not None:
+            course_org = obj.course_org
+            course_org.course_nums = Course.objects.filter(course_org=course_org).count()
+            course_org.save()
 
 
 class BannerCourseAdmin(object):
--- apps/courses/models.py
+++ apps/courses/models.py
@@ -1,6 +1,7 @@
 # _*_ coding:utf-8 _*_
 from __future__ import unicode_literals
 from datetime import datetime
+from DjangoUeditor.models import UEditorField
 
 from django.db import models
 from organization.models import CourseOrg, Teacher
@@ -14,7 +15,8 @@ class Course(models.Model):
     teacher = models.ForeignKey(Teacher, verbose_name=u"讲师", null=True, blank=True)
     name = models.CharField(max_length=50, verbose_name=u"课程名")
     desc = models.CharField(max_length=300, verbose_name=u"课程描述")
-    detail = models.TextField(verbose_name=u"课程详情")
+    detail = UEditorField(verbose_name=u"课程详情", width=600, height=300, imagePath="courses/ueditor/",
+                          filePath="courses/ueditor/", default='')
     is_banner = models.BooleanField(default=False, verbose_name=u"是否轮播")
     degree = models.CharField(verbose_name=u'难度', choices=(("cj", u"初级"), ("zj", u"中级"), ("gj", u"高级")), max_length=2)
     learned_times = models.IntegerField(default=0, verbose_name=u"学习时长(分钟数)")
@@ -36,6 +38,14 @@ class Course(models.Model):
     def get_zj_nums(self):
         all_lessons = self.lesson_set.all().count()
         return all_lessons
+
+    get_zj_nums.short_description = u"章节数"
+
+    def go_to(self):
+        from django.utils.safestring import mark_safe
+        return mark_safe("<a href='http://lawtech0902.com'>跳转</a>")
+
+    go_to.short_description = u"跳转"
 
     def get_learn_users(self):
         return self.usercourse_set.all()[:5]
